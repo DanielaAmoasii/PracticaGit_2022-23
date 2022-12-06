442a24cedd353e559101fabda7739a26daa26c4d
OlgaFCB <olgafcb_@hotmail.com>
2022-12-06 20:06:05 +0100

    Corrección del código

--- PracticaGit2022-23DAM-OFCB/Form1.Designer.cs
+++ PracticaGit2022-23DAM-OFCB/Form1.Designer.cs
@@ -40,27 +40,27 @@
             // 
             // txtPrecio
             // 
-            this.txtPrecio.Location = new System.Drawing.Point(96, 299);
-            this.txtPrecio.Margin = new System.Windows.Forms.Padding(4);
+            this.txtPrecio.Location = new System.Drawing.Point(128, 368);
+            this.txtPrecio.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtPrecio.Name = "txtPrecio";
-            this.txtPrecio.Size = new System.Drawing.Size(132, 20);
+            this.txtPrecio.Size = new System.Drawing.Size(175, 22);
             this.txtPrecio.TabIndex = 11;
             // 
             // txtTelegrama
             // 
-            this.txtTelegrama.Location = new System.Drawing.Point(41, 110);
-            this.txtTelegrama.Margin = new System.Windows.Forms.Padding(4);
+            this.txtTelegrama.Location = new System.Drawing.Point(55, 135);
+            this.txtTelegrama.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.txtTelegrama.Multiline = true;
             this.txtTelegrama.Name = "txtTelegrama";
-            this.txtTelegrama.Size = new System.Drawing.Size(471, 113);
+            this.txtTelegrama.Size = new System.Drawing.Size(627, 138);
             this.txtTelegrama.TabIndex = 10;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(357, 272);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Location = new System.Drawing.Point(476, 335);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(155, 46);
+            this.button1.Size = new System.Drawing.Size(207, 57);
             this.button1.TabIndex = 9;
             this.button1.Text = "Calcular";
             this.button1.UseVisualStyleBackColor = true;
@@ -69,22 +69,23 @@
             // cbUrgente
             // 
             this.cbUrgente.AutoSize = true;
-            this.cbUrgente.Location = new System.Drawing.Point(41, 251);
-            this.cbUrgente.Margin = new System.Windows.Forms.Padding(4);
+            this.cbUrgente.Location = new System.Drawing.Point(55, 309);
+            this.cbUrgente.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.cbUrgente.Name = "cbUrgente";
-            this.cbUrgente.Size = new System.Drawing.Size(70, 17);
+            this.cbUrgente.Size = new System.Drawing.Size(84, 20);
             this.cbUrgente.TabIndex = 8;
             this.cbUrgente.Text = "Urgente?";
             this.cbUrgente.UseVisualStyleBackColor = true;
+            this.cbUrgente.CheckedChanged += new System.EventHandler(this.cbUrgente_CheckedChanged);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(37, 302);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Location = new System.Drawing.Point(49, 372);
+            this.label2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.Size = new System.Drawing.Size(49, 17);
             this.label2.TabIndex = 7;
             this.label2.Text = "Coste";
             // 
@@ -92,10 +93,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(37, 79);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(49, 97);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.Size = new System.Drawing.Size(48, 17);
             this.label1.TabIndex = 6;
             this.label1.Text = "Texto";
             // 
@@ -104,9 +105,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Maiandra GD", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.Sienna;
-            this.label3.Location = new System.Drawing.Point(93, 47);
+            this.label3.Location = new System.Drawing.Point(124, 58);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(389, 16);
+            this.label3.Size = new System.Drawing.Size(493, 20);
             this.label3.TabIndex = 12;
             this.label3.Text = "Daniela Amoasii Marin y Olga F. Civieta Bermejo 2022/23";
             // 
@@ -115,17 +117,18 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Maiandra GD", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Maroon;
-            this.label4.Location = new System.Drawing.Point(181, 9);
+            this.label4.Location = new System.Drawing.Point(241, 11);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(182, 22);
+            this.label4.Size = new System.Drawing.Size(226, 29);
             this.label4.TabIndex = 13;
             this.label4.Text = "PRÁCTICA GIT 3.1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(574, 366);
+            this.ClientSize = new System.Drawing.Size(765, 450);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtPrecio);
@@ -134,6 +137,7 @@
             this.Controls.Add(this.cbUrgente);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Práctica GIT 3.1";
             this.ResumeLayout(false);
